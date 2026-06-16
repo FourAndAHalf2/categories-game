8fe3f0596fd3ac21bc49b4439664dfd027f1ed56
9.5 <fourandahalf2@gmail.com>
2026-06-16 18:54:27 +0200

    added caching of restcountries

--- backend/cached_get.py
+++ backend/cached_get.py
@@ -1,43 +1,63 @@
 from requests import get
-from requests import Response
 from datetime import datetime, timedelta
 from hashlib import sha256
 from os.path import isfile
 from pathlib import Path
 from json import load, dump
+from time import time
+from glob import glob
 
 cache = {}
 
 
 def cached_get(url: str, cache_duration=10, headers: dict | None = None) -> str:
-    """# Fetching and caching dynamic data of a website
-
-    Args:
-        url (str): url of website to fetch
-        cache_duration (int, optional): time (in minutes) in cached data is used in caching. Defaults to 10.
-        headers (dict | None, optional): headers used while fetching data. Defaults to None.
-
-    Returns:
-        str: _description_
-    """
-
     if headers is None:
         headers = {}
 
     now = datetime.now()
+
+
     entry = cache.get(url)
+    if entry and entry["expiry"] > now:
+        return entry["content"]
 
-    # return valid cached response
-    if entry is not None and entry.get("expiry") is not None and entry["expiry"] > now:
-        return entry["response"]
+    hash_of_url = sha256(url.encode()).hexdigest()
 
+    cache_directory = Path(__file__).parent / "data" / "cache"
+    cache_directory.mkdir(parents=True, exist_ok=True)
+
+
+    for file_path in cache_directory.glob(f"{hash_of_url}.*"):
+        try:
+            timestamp = int(file_path.suffix[1:])
+        except ValueError:
+            continue
+
+        if timestamp > time():
+            content = file_path.read_text(encoding="utf-8")
+
+            cache[url] = {
+                "content": content,
+                "expiry": datetime.fromtimestamp(timestamp),
+            }
+
+            return content
+        else:
+            file_path.unlink(missing_ok=True)
+
+    # Pobieranie z internetu
     response = get(url, headers=headers)
+    response.raise_for_status()
 
-    if response.status_code == 200:
-        cache[url] = {
-            "response": response.text,
-            "expiry": now + timedelta(minutes=cache_duration),
-        }
+    expiry_timestamp = int(time() + 60 * cache_duration)
+    cache_file_path = cache_directory / f"{hash_of_url}.{expiry_timestamp}"
+
+    cache_file_path.write_text(response.text, encoding="utf-8")
+
+    cache[url] = {
+        "content": response.text,
+        "expiry": now + timedelta(minutes=cache_duration),
+    }
 
     return response.text
 
@@ -60,9 +80,7 @@ def cached_get_static(url: str, headers: dict | None = None) -> str:
         with open(path) as file:
             data = load(file)
     else:
-        response = get(
-            url
-        )
+        response = get(url, headers=headers)
 
         data = response.text
         with open(path, "w") as file:
